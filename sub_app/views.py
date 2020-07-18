a65b57fa2faa1be075677b28819bc31b42bff9f5
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2020-07-18 21:24:44 +0500

    image display issue resolved

--- sub_app/views.py
+++ sub_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from .models import image_classification
 from django.http import HttpResponseRedirect
-
+from django.conf import settings
 # Create your views here.
 def home(request):
 	
 	images=image_classification.objects.all()
 	url=images[len(images)-1].pic.url
-	print('here u go',url)
+	print('here u go',settings.MEDIA_ROOT+url)
 	return render(request,'home.html',{'print':"every thing ok",'image':url})
 
 def uploadImage(request):
