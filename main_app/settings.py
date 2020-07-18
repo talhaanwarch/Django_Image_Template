a6e10458af1740f4951de0405d5ed28a32a4152d
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2020-07-18 19:27:21 +0500

    updated the urls.py

--- main_app/settings.py
+++ main_app/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'main_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT =  os.path.join(BASE_DIR,'sub_app', 'media')
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
